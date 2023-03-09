167021c45f5327704664143a89e679d11f8dbd2a
UnSpoken12 <71053495+kongshijatwit@users.noreply.github.com>
2023-03-09 00:44:04 -0500

    Refactored NPC spawn in hierarchy and script

--- WatchOutGame_12_10_2021/Assets/Scripts/NPC/RandomSpawner.cs
+++ WatchOutGame_12_10_2021/Assets/Scripts/NPC/RandomSpawner.cs
@@ -8,18 +8,20 @@ public class RandomSpawner : MonoBehaviour
     [SerializeField] private GameObject npcPrefab;
     [SerializeField] private float spawnerRadius = 1;
     [SerializeField] private int spawnAmount;
-
+    
     void Start()
     {
-        for (int i = 0; i < spawnAmount; i++)
+        for (int i = 0; i < spawnAmount; i++) 
         {
             Vector2 randomPos = (Vector2)transform.position + Random.insideUnitCircle * spawnerRadius;
             Instantiate(npcPrefab, randomPos, Quaternion.identity);
         }
     }
 
-    //Shows an outline of the spawn radius
-    private void OnDrawGizmos()
+    /// <summary>
+    /// Displays outline of spawn radius
+    /// </summary>
+    private void OnDrawGizmos() 
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(this.transform.position, spawnerRadius);
