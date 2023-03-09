98b235bc0abf1a9bbd7db34865d2f5f4bd0b605d
UnSpoken12 <71053495+kongshijatwit@users.noreply.github.com>
2023-03-09 01:33:51 -0500

    Refactored scene transitioning scripts

--- WatchOutGame_12_10_2021/Assets/Scripts/ChangeScene.cs
+++ WatchOutGame_12_10_2021/Assets/Scripts/ChangeScene.cs
@@ -1,38 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-//Class used to go from scene to scene, transitioning between "levels"
-
+/// <summary>
+/// Handles transitioning between levels and stores player position. 
+/// Should be placed on doors and entrances rather than the player itself
+/// </summary>
 public class ChangeScene : MonoBehaviour
 {
-    public string sceneName;
-    public Vector2 playerPosition;
-    public VectorValue playerStorage;
-    public Inventory playerInventory;
-    public bool initialize;    
+    [SerializeField] private string sceneName;
+    [SerializeField] private VectorValue playerStorage;
+    [SerializeField] private Inventory playerInventory;
+    [SerializeField] private Vector2 playerPosition;
+    [SerializeField] private bool initialize;
 
-    //Method for changing the scene when clicking a button (Ex: going from the shop back to the pharmacy)
-    public void changeSceneTo(string sceneName) {
+    /// <summary>
+    /// Changes the scene to another based on its name
+    /// </summary>
+    /// <param name="sceneName">Name of the scene being transitioned to</param>
+    public void ChangeSceneTo(string sceneName)
+    {
+        // -- I don't know why this is here -- //
         if(initialize) //basically if the scene change
         {
             playerInventory.clear();
         }
+        // -- End of I Don't Know segment -- //
+
         playerStorage.initialValue = playerPosition;
         SceneManager.LoadScene(sceneName);
     }
     
-    //Method for changing the scene when the player walks into an object (Ex: walking into a door to leave a building)
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        //Checks if object colliding is the player based on the object's tag
+        // In case an NPC triggers we check for the Player tag
         if (collision.CompareTag("Player") && !collision.isTrigger)
         {
-            changeSceneTo(sceneName);
-            
+            ChangeSceneTo(sceneName);
         }
     }
-
 }
--- WatchOutGame_12_10_2021/Assets/Scripts/LevelManager.cs
+++ WatchOutGame_12_10_2021/Assets/Scripts/LevelManager.cs
@@ -1,15 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-//Attempt to change level while storing player position
+/// <summary>
+/// Changes scenes while also saving player position
+/// </summary>
 public class LevelManager : MonoBehaviour
 {
-   public Transform player;
-   private Dictionary<int, Vector3> startingPosition = new Dictionary<int, Vector3>();
-    
-    void Start(){
+   [SerializeField] private Transform player;
+   private Dictionary<int, Vector3> startingPosition = new();
+
+    private void Awake()
+    {
+        SceneManager.sceneLoaded += (Scene s, LoadSceneMode m) =>
+        {
+            if (startingPosition.ContainsKey(s.buildIndex)) player.position = startingPosition[s.buildIndex];
+        };
         DontDestroyOnLoad(gameObject);
     }
 
@@ -17,9 +23,4 @@ public class LevelManager : MonoBehaviour
         startingPosition[SceneManager.GetActiveScene().buildIndex] = player.position;
         SceneManager.LoadScene(level);
     }
- 
-    public void OnLevelWasLoaded(int level) {
-        if (startingPosition.ContainsKey(level)) player.position = startingPosition[level];
-    }
 }
-
